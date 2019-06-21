91e0e0d282f5813029646a610acda6209f866a5f
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-21 10:00:32 +1000

    add CFG for key controls

---
+++ src/CFG.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#ifndef CFG_H
+#define CFG_H
+
+#include "Text.h"
+#include "MenuManager.h"
+#include "Music.h"
+
+class CCFG
+{
+private:
+    static MenuManager* oMM;
+    static Text* oText;
+    static CIMG* tSMBLOGO;
+    static Music* oMusic;
+
+public:
+    CCFG(void);
+    ~CCFG(void);
+
+    static int GAME_WIDTH;
+    static int GAME_HEIGHT;
+
+    static bool keySpace;
+
+    static int keyIDA;
+    static int keyIDS;
+    static int keyIDD;
+    static int keyIDSpace;
+    static int keyIDShift;
+
+    static CIMG* getSMBLOGO();
+
+    static Text* getText();
+
+    static MenuManger* getMM();
+    static Music* getMusic();
+
+    static bool canMoveBackward;
+};
