243694ef8136da3ff2dbb1c97877ce4e73850398
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-21 14:27:24 +1000

    add aboutmenu

---
+++ src/AboutMenu.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#ifndef ABOUTMENU_H
+#define ABOUTMENU_H
+
+#include "Menu.h"
+
+class AboutMenu : public Menu
+{
+private:
+    unsigned int iTime;
+
+    int cR;
+    int cG
+    int cB;
+    int nR;
+    int nG;
+    int nB;
+
+    int colorStepID;
+    int iColorID;
+
+    // true = RIGHT, false = LEFT
+    bool moveDirection;
+
+    int iNumOfUnits;
+
+public:
+    AboutMenu(void);
+    ~AboutMenu(void);
+
+    void Update();
+    void Draw(SDL_Renderer* rR);
+    
+    void enter();
+    void launch();
+    void reset();
+
+    void nextColor();
+    int getColorStep(int iOld, int iNew);
+
+    void setBackgroudColor(SDL_Renderer* rR);
+    void updateTime();
+}
+
+#endif // !ABOUTMENU_H
