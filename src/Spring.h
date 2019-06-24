05c19aa6a853c642b43d5b1f438a95c685f3b9ca
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-24 11:49:19 +1000

    add spring header

---
+++ src/Spring.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef SPRING_H
+#define SPRING_H
+
+#include "Minon.h"
+
+class Spring: public Minion
+{
+private:
+    bool inAnimation;
+    int nextFrameID;
+    bool extraJump;
+public:
+    Spring(int iXPos, int iYPos);
+    ~Spring(void);
+
+    void Update();
+    void Draw(SDL_Renderer* rR, CIMG* iIMG);
+
+    void minionPhysics();
+    void collisionWithPlayer(bool TOP);
+    void setMinionState(int minionState);
+}
+
+#endif // !SPRING_H
