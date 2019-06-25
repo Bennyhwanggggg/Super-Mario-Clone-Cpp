c0d9a5edb1258dd95ec1df646e81462f4625ca83
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-25 10:24:39 +1000

    add menu classes

---
+++ src/LoadingMenu.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef LOADINGMENU_H
+#define LOADINGMENU_H
+
+#include "Menu.h"
+
+class LoadingMenu: public Menu
+{
+private:
+    unsigned int iTime;
+public:
+    LoadingMenu(void);
+    ~LoadingMenu(void);
+
+    void Update();
+    void Draw(SDL_Renderer* rR);
+
+    void updateTime();
+
+    // -- true = LOADING, false = GAME OVER
+    bool loadingType;
+}
+
+#endif // !LOADINGMENU_H
