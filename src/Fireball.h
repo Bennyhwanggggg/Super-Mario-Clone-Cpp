f9ce43c41a787d635dc9809504a7f4645f70252c
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-27 09:36:07 +1000

    fireball header

---
+++ src/Fireball.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef FIREBALL_H
+#define FIREBALL_H
+
+#include "Minion.h"
+
+class FireBall: public Minion
+{
+private:
+    int radius;
+    double angle;
+    int iCenterX;
+    int iCenterY;
+
+    double slice;
+    int sliceID;
+public:
+    FireBall(int iXPos, int iYPos, int radius, int nSliceID, bool moveDirection);
+    ~FireBall(void);
+
+    void Draw(SDL_Renderer* rR, CIMG* iIMG);
+    void Update();
+    void updateXPos();
+    void collisionWithPlayer(bool TOP);
+    void minionPhysics();
+}
+
+#endif // !FIREBALL_H
