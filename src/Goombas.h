1f07a439aa7feee091f3e1ef7a44affd69c096e0
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-27 14:01:09 +1000

    start goombas

---
+++ src/Goombas.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef GOOMBAS_H
+#define GOOMBAS_H
+
+#include "Minion.h"
+
+class Goombas: public Minion
+{
+private:
+
+public:
+    Goombas(int iX, int iY, int iBlockID, bool moveDirection);
+    ~Goombas(void);
+
+    void Update();
+    void Draw(SDL_Renderer* rR, CIMG* iIMG);
+    void collisionWithPlayer(bool TOP);
+    void setMinionState(int minionState);
+}
+
+#endif // ! GOOMBAS_H
