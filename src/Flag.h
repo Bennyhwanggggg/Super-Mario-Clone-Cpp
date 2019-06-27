fa46d6d01bf0e34b869be8043b490f82a3ba7d49
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-27 10:04:16 +1000

    add finishing flag to game

---
+++ src/Flag.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#ifndef FLAG_H
+#define FLAG_H
+
+#include "header.h"
+#include "IMG.h"
+
+class Flag
+{
+private:
+
+public:
+    Flag(int iXPos, int iYPos);
+    ~Flag(void);
+
+    int iXPos;
+    int iYPos;
+    int iBlockID;
+
+    int iYTextPos;
+    int iPoints;
+
+    int castleFlagExtraXPos;
+    int castleFlagY
+
+    void Update();
+    void UpdateCastleFlag();
+    void Draw(SDL_Renderer* rR, CIMG* iIMG);
+    void DrawCastleFlag(SDL_Renderer* rR, CIMG* iIMG);
+}
+
+#endif // !FLAG_H
