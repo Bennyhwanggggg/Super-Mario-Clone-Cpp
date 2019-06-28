5ecd90d9b2f57da9f09847da6ada832d19c91a0c
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-28 10:08:39 +1000

    start plant;

---
+++ src/Plant.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef PLANT_H
+#define PLANT_H
+
+#include "Minion.h"
+
+class Plant: public Minion
+{
+private:
+    bool bWait;
+    unsigned int iTime;
+    bool lockPlant;
+    int iLength;
+    int X;
+    int Y;
+public:
+    Plant(int fXPos, int fYPos, int iBlockID);
+    ~Plant(void);
+    void Update();
+    void Draw(SDL_Renderer* rR, CIMG* iIMG);
+    void minionPhysics();
+	void collisionWithPlayer(bool TOP);
+	void collisionEffect();
+	void lockMinion();
+	void setMinionState(int minionState);
+}
+
+#endif // !PLANT_H
